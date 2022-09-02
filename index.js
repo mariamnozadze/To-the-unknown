67dac601957cd34567544d692d42d1f338d16be7
mariamnozadze <mariamnozadze97@yahoo.com>
2022-09-02 16:38:43 +0400

    - Added gsap

---
+++ index.js
@@ -0,0 +1,3 @@
+gsap.from(".top", { opacity: 0, duration: 1, delay: 0.5, y: -10 });
+gsap.from(".center", { opacity: 0, duration: 1, delay: 1, y: -10 });
+gsap.from(".hero-desc", { opacity: 0, duration: 1, delay: 1.5, y: -10 });
